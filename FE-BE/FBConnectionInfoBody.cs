eb17ef337b92ef7af480124ad14845dcea9ac769
JinHyuk-Lee <obital59@gmail.com>
2016-08-19 09:09:27 +0100

    2016-08-19 Version

---
+++ FE-BE/FBConnectionInfoBody.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Runtime.InteropServices;
+
+public struct FBConnectionInfoBody
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
+    public char[] ip;
+    public int port; //lenght of next body 
+
+    Protocol protocol;
+
+    public char[] Ip
+    {
+        get { return this.ip; }
+        set { this.ip = value; }
+    }
+
+    public int Port
+    {
+        get { return this.port; }
+        set { this.port = value; }
+    }
+}
---
+++ FE-BE/FBCookieRunBody.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+    public struct FBCookieRunBody
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+        public char[] id;
+        public int cookie;
+    }
+
